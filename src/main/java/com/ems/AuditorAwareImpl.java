8f4de19e0377bcc769dca6daa630e999424be75b
abkp009 <abkp009@gmail.com>
2018-09-15 18:11:34 +0530

    added java doc

--- src/main/java/com/ems/AuditorAwareImpl.java
+++ src/main/java/com/ems/AuditorAwareImpl.java
@@ -3,7 +3,7 @@ package com.ems;
 import java.util.Optional;
 
 import org.springframework.data.domain.AuditorAware;
-
+// generic class for auditing (Entity)
 public class AuditorAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
